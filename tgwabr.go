018a0f8c4b2ac2adb1be8fa7c33d3e362b4cc423
Artem V. Zaborskiy <ftomza@yandex.ru>
2020-02-05 14:58:22 +0300

    Change init process

--- tgwabr.go
+++ tgwabr.go
@@ -1,1 +1,63 @@
 package tgwabr
+
+import (
+	"context"
+	"log"
+	appCtx "tgwabr/context"
+	"tgwabr/pkg/cache"
+	"tgwabr/pkg/store"
+	"tgwabr/pkg/tg"
+	"tgwabr/pkg/wa"
+)
+
+func Init() func() {
+	var (
+		err       error
+		waImpl    *wa.Service
+		tgImpl    *tg.Service
+		storeImpl *store.Store
+		cacheImpl *cache.Cache
+	)
+
+	ctx := context.Background()
+
+	if storeImpl, err = store.New(ctx); err != nil {
+		log.Fatalln("Fail Store Instance: ", err)
+	}
+
+	ctx = appCtx.NewDB(ctx, storeImpl)
+
+	if waImpl, err = wa.New(ctx); err != nil {
+		log.Fatalln("Fail WA Instance: ", err)
+	}
+
+	ctx = appCtx.NewWA(ctx, waImpl)
+
+	if tgImpl, err = tg.New(ctx); err != nil {
+		log.Fatalln("Fail TG Instance: ", err)
+	}
+
+	ctx = appCtx.NewTG(ctx, tgImpl)
+
+	if cacheImpl, err = cache.New(ctx, cache.Config{GetMembers: tgImpl.GetMembers}); err != nil {
+		log.Fatalln("Fail Cache Instance: ", err)
+	}
+
+	ctx = appCtx.NewCache(ctx, cacheImpl)
+	waImpl.UpdateCTX(ctx)
+
+	return func() {
+		if err = waImpl.ShutDown(); err != nil {
+			log.Println("Fail WA Instance: ", err)
+		}
+		if err = tgImpl.ShutDown(); err != nil {
+			log.Println("Fail TG Instance: ", err)
+		}
+		if err = storeImpl.ShutDown(); err != nil {
+			log.Println("Fail Store Instance: ", err)
+		}
+		if err = cacheImpl.ShutDown(); err != nil {
+			log.Println("Fail Store Instance: ", err)
+		}
+	}
+}
