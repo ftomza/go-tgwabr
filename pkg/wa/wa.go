bd871ad90088227ec06c393e280eea7366b61d7b
Artem V. Zaborskiy <ftomza@yandex.ru>
2020-02-06 10:16:46 +0300

    Stable login and reconnect

--- pkg/wa/wa.go
+++ pkg/wa/wa.go
@@ -3,6 +3,7 @@ package wa
 import (
 	"context"
 	"encoding/gob"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -61,56 +62,38 @@ func (s *Service) ShutDown() error {
 }
 
 func (s *Service) login(onlyRestore bool) error {
-	session, err := s.readSession()
-	if err == nil && session.ClientId != "" {
-		session, err = s.conn.RestoreWithSession(session)
-		if err != nil {
-			if errSave := s.writeSession(session); errSave != nil {
-				return fmt.Errorf("error saving session: %v", errSave)
-			}
-			if !onlyRestore {
-				return fmt.Errorf("restoring session failed: %w. Please try again. ", err)
-			}
-		}
+
+	var ok bool
+	var err error
+
+	if ok, err = s.restore(); err != nil && !onlyRestore {
+		return fmt.Errorf("error restore: %w", err)
+	} else if err != nil {
+		log.Println("error restore: ", err)
 	}
-	if !onlyRestore {
-		qr := make(chan string)
-		tg, _ := appCtx.FromTG(s.ctx)
-		var tgMsg *api.TGMessage
-		go func() {
-			if tg == nil {
-				tg, _ = appCtx.FromTG(s.ctx)
-			}
-			tgMsg, err = tg.SendQR(<-qr)
-			if err != nil {
-				log.Printf("error send QR: %v\n", err)
-			}
-		}()
-		session, err = s.conn.Login(qr)
-		if tgMsg != nil {
-			err = tg.DeleteMessage(tgMsg.ChatID, tgMsg.MessageID)
-			if err != nil {
-				return fmt.Errorf("error delete tg message: %w", err)
-			}
+
+	if !ok {
+		if ok, err = s.restoreSession(); err != nil && !onlyRestore {
+			return fmt.Errorf("error restore session: %w", err)
 		}
-		if err != nil {
-			return fmt.Errorf("error during login: %w", err)
+	} else if err != nil {
+		log.Println("error restore session: ", err)
+	}
+
+	if !ok && !onlyRestore {
+		if ok, err = s.loginSession(); err != nil {
+			return fmt.Errorf("error restore session: %w", err)
 		}
 	}
 
-	pong, err := s.conn.AdminTest()
+	ok, err = s.conn.AdminTest()
 	if err != nil {
-		err = s.conn.Restore()
-		if err != nil {
-			session, _ = s.conn.Disconnect()
+		if !onlyRestore {
+			return fmt.Errorf("error ping: %w", err)
 		}
 	}
 
-	log.Println("WA Status: ", pong)
-
-	if err = s.writeSession(session); err != nil {
-		return fmt.Errorf("error saving session: %v", err)
-	}
+	log.Println("WA Status: ", ok)
 
 	versionServer, err := whatsapp.CheckCurrentServerVersion()
 	if err != nil {
@@ -123,6 +106,67 @@ func (s *Service) login(onlyRestore bool) error {
 	log.Printf("whatsapp version %v.%v.%v", versionClient[0], versionClient[1], versionClient[2])
 
 	return nil
+}
+
+func (s *Service) restore() (ok bool, err error) {
+	err = s.conn.Restore()
+	if err != nil && (errors.Is(err, whatsapp.ErrAlreadyConnected) || errors.Is(err, whatsapp.ErrAlreadyLoggedIn)) {
+		return true, nil
+	} else if err != nil && (errors.Is(err, whatsapp.ErrInvalidSession)) {
+		return false, nil
+	}
+	return true, nil
+}
+
+func (s *Service) restoreSession() (ok bool, err error) {
+	session, err := s.readSession()
+	if err == nil && session.ClientId != "" {
+		session, err = s.conn.RestoreWithSession(session)
+		if err != nil && (errors.Is(err, whatsapp.ErrAlreadyConnected) || errors.Is(err, whatsapp.ErrAlreadyLoggedIn)) {
+			return true, nil
+		} else if err != nil && (errors.Is(err, whatsapp.ErrInvalidSession)) {
+			return false, nil
+		}
+		if err != nil {
+			if errSave := s.writeSession(session); errSave != nil {
+				return false, fmt.Errorf("error saving session: %v", errSave)
+			}
+			return false, fmt.Errorf("restoring session failed: %w. Please try again. ", err)
+		}
+	}
+
+	return true, nil
+}
+
+func (s *Service) loginSession() (ok bool, err error) {
+	qr := make(chan string)
+	tg, _ := appCtx.FromTG(s.ctx)
+	var tgMsg *api.TGMessage
+	go func() {
+		if tg == nil {
+			tg, _ = appCtx.FromTG(s.ctx)
+		}
+		tgMsg, err = tg.SendQR(<-qr)
+		if err != nil {
+			log.Printf("error send QR: %v\n", err)
+		}
+	}()
+	session, err := s.conn.Login(qr)
+	if tgMsg != nil {
+		err = tg.DeleteMessage(tgMsg.ChatID, tgMsg.MessageID)
+		if err != nil {
+			return false, fmt.Errorf("error delete tg message: %w", err)
+		}
+	}
+	if err != nil {
+		return false, fmt.Errorf("error during login: %w", err)
+	}
+	if errSave := s.writeSession(session); errSave != nil {
+		_ = s.conn.Logout()
+		_, _ = s.conn.Disconnect()
+		return false, fmt.Errorf("error saving session: %v", errSave)
+	}
+	return true, nil
 }
 
 func (s *Service) readSession() (whatsapp.Session, error) {
